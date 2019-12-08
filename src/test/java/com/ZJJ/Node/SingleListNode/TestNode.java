d4c8610e8b516dce8ac13a33118ef91e8dd08e0b
zz Jenkin <1277874614@qq.com>
2019-12-08 22:04:57 +0800

    为单链表增加了更多功能

--- src/test/java/com/ZJJ/Node/SingleListNode/TestNode.java
+++ src/test/java/com/ZJJ/Node/SingleListNode/TestNode.java
@@ -1,25 +1,28 @@
 package com.ZJJ.Node.SingleListNode;
 
+import com.ZJJ.Node.Node;
 import org.junit.Test;
+
 
 public class TestNode {
     @Test
+    public void TestNode() throws Exception {
+        Node<Integer> node = new Node<>();
+        System.out.println(node);
+    }
+
+    @Test
     public void NodeTest() throws Exception {
-        SingleListNode<String> first = new SingleListNode<>();
-        first.value="1";
-        SingleListNode<String> second = new SingleListNode<>();
-        second.value="2";
-        SingleListNode<String> third = new SingleListNode<>();
-        third.value="3";
-        SingleListNode<String> forth = new SingleListNode<>();
-        forth.value="4";
-        SingleListNode<String> fifth = new SingleListNode<>();
-        fifth.value="5";
-        first.next=second;
-        second.next=third;
-        third.next=forth;
-        forth.next=fifth;
-        System.out.println(first.value+" "+first.next.value);
-        System.out.println(first);
+        SingleListNode<String> listNode = new SingleListNode<>(new Node("1"));
+        listNode.updateNode("2",0);
+        listNode.addNode("0",0);
+        listNode.addNode("1",1);
+        listNode.addNode("9",99);
+        System.out.println(listNode);
+        listNode.deleteNode(0);
+        System.out.println(listNode.findNode(0));
+        listNode.addNode("9",99);
+        listNode.addNode("7",3);
+        System.out.println(listNode);
     }
 }
