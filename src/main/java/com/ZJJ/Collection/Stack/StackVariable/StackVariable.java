2c30e25fc8dab5ea265daac193ae80b6d463c53e
zz Jenkin <1277874614@qq.com>
2019-12-08 14:15:59 +0800

    Initial commit

---
+++ src/main/java/com/ZJJ/Collection/Stack/StackVariable/StackVariable.java
@@ -0,0 +1,93 @@
+package com.ZJJ.Collection.Stack.StackVariable;
+
+import java.util.Arrays;
+import java.util.Iterator;
+
+public class StackVariable<T> implements Iterable<T> {
+
+    private T[] values;
+
+    private int size;
+
+    private int capacity;
+
+    public StackVariable(){
+        this(16);
+    }
+
+    public StackVariable(int capacity) {
+        values = (T[]) new Object[capacity];
+        size = 0;
+        this.capacity = capacity;
+    }
+
+    private void resize(int newCapacity){
+        T[] temp = (T[]) new Object[newCapacity];
+        System.arraycopy(values,0,temp,0,size);
+        values = temp;
+        capacity = newCapacity;
+    }
+
+    public void push(T value){
+        if(size == capacity){
+            resize(2*capacity);
+        }
+        values[size++] = value;
+    }
+
+    public T pop(){
+        if(isEmpty()) {
+            throw new RuntimeException("栈已空，无法删除元素！");
+        }
+        if(size > 0 && size == capacity/4){
+            resize(capacity/2);
+        }
+        T value = values[--size];
+        values[size] = null;
+        return value;
+    }
+
+    public T peek(){
+        if(isEmpty()) {
+            throw new RuntimeException("栈为空！");
+        }
+        return values[size-1];
+    }
+
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public int capacity() {
+        return capacity;
+    }
+
+    @Override
+    public String toString() {
+        return "Stack{" +
+                "values=" + Arrays.toString(values) +
+                ", size=" + size +
+                ", capacity=" + capacity +
+                '}';
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            private int i=0;
+            @Override
+            public boolean hasNext() {
+                return i<size;
+            }
+
+            @Override
+            public T next() {
+                return values[i++];
+            }
+        };
+    }
+}
---
+++ src/test/java/com/ZJJ/Collection/Stack/StackTest.java
@@ -0,0 +1,49 @@
+package com.ZJJ.Collection.Stack;
+
+import com.ZJJ.Collection.Stack.StackFixed.StackFixed;
+import com.ZJJ.Collection.Stack.StackVariable.StackVariable;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+public class StackTest {
+    @Test
+    public void StackString() throws Exception {
+        StackFixed<String> stringStack = new StackFixed<>();
+        System.out.println(stringStack.capacity());
+        for(int i=0;i<16;i++){
+            stringStack.push("String "+i);
+        }
+        System.out.println(stringStack.pop());
+        System.out.println(stringStack);
+        System.out.println(stringStack.peek());
+    }
+
+    @Test
+    public void StackInteger() throws Exception {
+        StackFixed<Integer[]> stringStack = new StackFixed<>(42);
+        System.out.println(stringStack.capacity());
+        for(int i=0;i<16;i++){
+            stringStack.push(new Integer[]{i});
+        }
+        System.out.println(stringStack.peek());
+    }
+
+    @Test
+    public void StackVar() throws Exception {
+        StackVariable<String> stringStack = new StackVariable<>();
+        System.out.println(stringStack.capacity());
+        for(int i=0;i<17;i++){
+            stringStack.push("V:"+i);
+        }
+        System.out.println(stringStack);
+        System.out.println(stringStack.capacity());
+        for(int i=0;i<17;i++){
+            stringStack.pop();
+            System.out.println(stringStack);
+            System.out.println(stringStack.capacity());
+        }
+        System.out.println(stringStack.peek());
+    }
+}
