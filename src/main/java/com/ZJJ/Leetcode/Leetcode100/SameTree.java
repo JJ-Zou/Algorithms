f6b90533e6baf97685f097a8bf818ed8b3f4ab3a
zz Jenkin <1277874614@qq.com>
2019-12-15 16:53:11 +0800

    leetcode

---
+++ src/main/java/com/ZJJ/Leetcode/Leetcode100/SameTree.java
@@ -0,0 +1,20 @@
+package com.ZJJ.Leetcode.Leetcode100;
+
+import com.ZJJ.TreeNode.TreeNode;
+
+/**
+ * 给定两个二叉树，编写一个函数来检验它们是否相同。
+ * 递归，判断两个数的根节点与左子树右子树是否全相同
+ */
+public class SameTree {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p == null && q == null){
+            return true;
+        }
+        if(p == null || q == null){
+            return false;
+        }
+        return p.val == q.val && isSameTree(p.left,q.left) &&
+                isSameTree(p.right,q.right);
+    }
+}
