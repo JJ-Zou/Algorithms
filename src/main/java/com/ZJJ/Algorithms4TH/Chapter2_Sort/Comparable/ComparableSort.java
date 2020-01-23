d913406ed3cfffdd4d4cf4a937ff7cbf68b95e4b
zz Jenkin <1277874614@qq.com>
2020-01-23 21:18:26 +0800

    算法第四版 排序

--- src/main/java/com/ZJJ/Algorithms4TH/Chapter2_Sort/Comparable/ComparableSort.java
+++ src/main/java/com/ZJJ/Algorithms4TH/Chapter2_Sort/Comparable/ComparableSort.java
@@ -27,6 +27,7 @@ public class ComparableSort {
                 break;
             }
             right--;
+            flag = true;
             for (int i = right; i > left; i--) {
                 if (less(a[i], a[i - 1])) {
                     swap(a, i - 1, i);
@@ -127,7 +128,7 @@ public class ComparableSort {
             return;
         }
         //归并结果放入a中
-        for (int i = left, p = left, q = mid; i < left; i++) {
+        for (int i = left, p = left, q = mid; i < right; i++) {
             if (q >= right || (p < mid && less(support[p], support[q]))) {
                 a[i] = support[p++];
             } else {
@@ -155,7 +156,7 @@ public class ComparableSort {
                     continue;
                 }
                 //每次归并前将需要归并的部分放入辅助数组
-                System.arraycopy(a,left,support,left,high-left);
+                System.arraycopy(a, left, support, left, high - left);
                 for (int i = left, p = left, q = mid; i < high; i++) {
                     if (q >= high || (p < mid && less(support[p], support[q]))) {
                         a[i] = support[p++];
@@ -164,6 +165,102 @@ public class ComparableSort {
                     }
                 }
             }
+        }
+    }
+
+    /**
+     * 快速排序
+     *
+     * @param a
+     */
+    public static void quickSort(Comparable[] a) {
+        quickSort(a, 0, a.length - 1);
+    }
+
+    private static void quickSort(Comparable[] a, int left, int right) {
+        if (right - left < 6) {
+            for (int i = left; i <= right; i++) {
+                for (int j = i; j > left && less(a[j], a[j - 1]); j--) {
+                    swap(a, j, j - 1);
+                }
+            }
+            return;
+        }
+        //二向切分
+//        int pivot = partition(a, left, right);
+        //三向切分
+        int low = left;
+        int high = right;
+        int i = left + 1;
+        Comparable v = a[left];
+        while (i <= high) {
+            int cmp = a[i].compareTo(v);
+            if (cmp < 0) {
+                swap(a, low++, i++);
+            } else if (cmp > 0) {
+                swap(a, i, high--);
+            } else {
+                i++;
+            }
+        }
+        quickSort(a, left, low - 1);
+        quickSort(a, high + 1, right);
+//        quickSort(a, left, pivot - 1);
+//        quickSort(a, pivot + 1, right);
+    }
+
+    private static int partition(Comparable[] a, int left, int right) {
+        int low = left;
+        int high = right + 1;
+        Comparable v = a[left];
+        while (true) {
+            while (less(a[++low], v)) {
+                if (low == right) {
+                    break;
+                }
+            }
+            while (less(v, a[--high])) {
+                if (high == left) {
+                    break;
+                }
+            }
+            if (low >= high) {
+                break;
+            }
+            swap(a, low, high);
+        }
+        swap(a, left, high);
+        return high;
+    }
+
+    /**
+     * 堆排序
+     *
+     * @param a
+     */
+    public static void heapSort(Comparable[] a) {
+        int length = a.length;
+        int mid = (length - 1) >>> 1;
+        for (int i = mid; i >= 0; i--) {
+            sink(a, i, length - 1);
+        }
+        while (length > 1) {
+            swap(a, 0, --length);
+            sink(a, 0, length - 1);
+        }
+    }
+
+    private static void sink(Comparable[] a, int left, int right) {
+        while (left < ((right + 1) >>> 1)) {
+            int max = (left << 1) + 1;
+            if (max < right && less(a[max], a[max + 1])) {
+                max++;
+            }
+            if (!less(a[left], a[max])) {
+                break;
+            }
+            swap(a, left, max);
+            left = max;
         }
     }
 
