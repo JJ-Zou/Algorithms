eeb003a1c3d619454909cdd6ae6dd24e28e02ef0
zz Jenkin <1277874614@qq.com>
2020-03-13 13:43:16 +0800

    KMP

---
+++ src/main/java/com/ZJJ/Kmp/KMP.java
@@ -0,0 +1,44 @@
+package com.ZJJ.Kmp;
+
+public class KMP {
+    private static int kmp(String str1, String str2) {
+        if (str1 == null || str1.length() == 0
+                || str2 == null || str2.length() == 0 || str1.length() < str2.length()) {
+            return -1;
+        }
+        int p1 = 0;
+        int p2 = 0;
+        int[] next = next(str2);
+        while (p1 < str1.length() && p2 < str2.length()) {
+            if (str1.charAt(p1) == str2.charAt(p2)) {
+                p1++;
+                p2++;
+            } else if (next[p2] == -1) {
+                p1++;
+            } else {
+                p2 = next[p2];
+            }
+        }
+        return p2 == str2.length() ? p1 - p2 : -1;
+    }
+
+    private static int[] next(String str) {
+        if (str.length() == 1) {
+            return new int[]{-1};
+        }
+        int[] next = new int[str.length()];
+        next[0] = -1;
+        next[1] = 0;
+        int cur = 2;
+        while (cur < str.length()) {
+            if (str.charAt(cur - 1) == next[cur - 1]) {
+                next[cur++] = next[cur - 1] + 1;
+            } else if (next[cur - 1] > 0) {
+                next[cur - 1] = next[next[cur - 1]];
+            } else {
+                next[cur++] = 0;
+            }
+        }
+        return next;
+    }
+}
