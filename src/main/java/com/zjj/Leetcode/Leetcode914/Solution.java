527df865d4fe833468dd837a2e4f43ab0584e6b3
1277874614@qq.com <Zjj19961109>
2020-08-27 16:24:20 +0800

    leetcode

---
+++ src/main/java/com/zjj/Leetcode/Leetcode914/Solution.java
@@ -0,0 +1,32 @@
+package com.zjj.Leetcode.Leetcode914;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+    public static boolean hasGroupsSizeX(int[] deck) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : deck) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        int gcd = -1;
+        for (int key : map.keySet()) {
+            if (gcd == -1) {
+                gcd = map.get(key);
+            } else {
+                gcd = gcd(gcd, map.get(key));
+            }
+        }
+        return gcd >= 2;
+    }
+
+    private static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        if (b > a) {
+            return gcd(b, a);
+        }
+        return gcd(b, a % b);
+    }
+}
