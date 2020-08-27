8bb6f942f23c9647901bd836228444cce8d109d7
1277874614@qq.com <Zjj19961109>
2020-08-27 20:50:17 +0800

    leetcode937

---
+++ src/main/java/com/zjj/Leetcode/Leetcode917/Solution.java
@@ -0,0 +1,32 @@
+package com.zjj.Leetcode.Leetcode917;
+
+public class Solution {
+    public String reverseOnlyLetters(String S) {
+        char[] ch = S.toCharArray();
+        int left = 0;
+        int len = ch.length;
+        int right = len - 1;
+        while (left < right) {
+            while (!Character.isAlphabetic(ch[left])) {
+                left++;
+                if (left >= right) {
+                    return new String(ch);
+                }
+            }
+            while (!Character.isAlphabetic(ch[right])) {
+                right--;
+                if (left >= right) {
+                    return new String(ch);
+                }
+            }
+            swap(ch, left++, right--);
+        }
+        return new String(ch);
+    }
+
+    private void swap(char[] ch, int a, int b) {
+        char t = ch[a];
+        ch[a] = ch[b];
+        ch[b] = t;
+    }
+}
--- src/test/java/com/zjj/test1/ex2.java
+++ src/test/java/com/zjj/test1/ex2.java
@@ -3,6 +3,7 @@ package com.zjj.test1;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 public class ex2 {
@@ -24,8 +25,8 @@ public class ex2 {
 
     @Test
     public void testSplit() {
-        String s = "a ";
-        System.out.println(Arrays.toString(s.split(" ")));
+        String s = "a a s d d f";
+        System.out.println(Arrays.toString(s.split(" ", 2)));
     }
 
     @Test
@@ -45,6 +46,28 @@ public class ex2 {
         System.out.println(list1 == list2);
         System.out.println(list1.equals(list2));
 
+    }
+
+    @Test
+    public void testString() {
+        char a = 'a';
+        char b = '*';
+        System.out.println(Character.isAlphabetic(a));
+        System.out.println(Character.isAlphabetic(b));
+    }
+
+    @Test
+    public void stringSort() {
+        String[] strings = new String[]{"a1 9 2 3 1", "g1 act car", "zo4 4 7", "ab1 off key dog", "a8 act zoo"};
+        Arrays.sort(strings, 0, 3,
+                (o1, o2) -> {
+                    int cmp = o1.substring(o1.indexOf(" ") + 1).compareTo(o2.substring(o2.indexOf(" ") + 1));
+                    if (cmp == 0) {
+                        return o1.substring(0, o1.indexOf(" ") + 1).compareTo(o2.substring(0, o2.indexOf(" ") + 1));
+                    }
+                    return cmp;
+                });
+        System.out.println(Arrays.toString(strings));
     }
 
 }
