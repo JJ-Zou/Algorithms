8d6f3abbc161014bb1b6379ade657cebfb8dedd5
1277874614@qq.com <Zjj19961109>
2020-08-27 23:19:04 +0800

    leetcode

--- src/test/java/com/zjj/test1/ex2.java
+++ src/test/java/com/zjj/test1/ex2.java
@@ -2,9 +2,7 @@ package com.zjj.test1;
 
 import org.junit.Test;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 public class ex2 {
     @Test
@@ -70,4 +68,21 @@ public class ex2 {
         System.out.println(Arrays.toString(strings));
     }
 
+    @Test
+    public void testString1() {
+        String s = "c";
+        s = op(s);
+        Queue<String> queue = new PriorityQueue<>((o1, o2) -> {
+            int h1 = Integer.parseInt(o1.substring(0, 2));
+            int h2 = Integer.parseInt(o2.substring(0, 2));
+            int m1 = Integer.parseInt(o1.substring(2));
+            int m2 = Integer.parseInt(o2.substring(2));
+            return (h2 > h1) ? 1 : (h2 == h1 ? Integer.compare(m2, m1) : -1);
+        });
+        System.out.println(s);
+    }
+    private String op(String s) {
+        s += "a";
+        return s;
+    }
 }
