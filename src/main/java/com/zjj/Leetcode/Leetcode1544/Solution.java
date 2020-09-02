79e664f019bfa96a267f09b342fa2ab0a35a7ed3
zz Jenkin <1277874614@qq.com>
2020-09-02 16:12:32 +0800

    leetcode

---
+++ src/main/java/com/zjj/Leetcode/Leetcode1544/Solution.java
@@ -0,0 +1,28 @@
+package com.zjj.Leetcode.Leetcode1544;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.makeGood("NAanorRoOrROwnTNW"));
+    }
+
+    public String makeGood(String s) {
+        Deque<Character> deque = new ArrayDeque<>();
+        for (char c : s.toCharArray()) {
+            if (!deque.isEmpty() && Math.abs(deque.peekLast() - c) == 'a' - 'A') {
+                deque.pollLast();
+            } else {
+                deque.addLast(c);
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        while (!deque.isEmpty()) {
+            sb.append(deque.pollFirst());
+        }
+        return sb.toString();
+    }
+}
