38ea3c35a1573ec767fe6fdc20d78859e36fcfbd
1277874614@qq.com <Zjj19961109>
2020-09-14 21:51:40 +0800

    leetcode

---
+++ src/main/java/com/zjj/Leetcode/Leetcode120/Solution.java
@@ -0,0 +1,90 @@
+package com.zjj.Leetcode.Leetcode120;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        List<List<Integer>> triangle = new ArrayList<>();
+        triangle.add(Arrays.asList(2));
+        triangle.add(Arrays.asList(3, 4));
+        triangle.add(Arrays.asList(6, 5, 7));
+        triangle.add(Arrays.asList(4, 1, 8, 3));
+        System.out.println(solution.minimumTotal(triangle));
+        System.out.println(solution.minimumTotal2(triangle));
+    }
+
+    /**
+     * 空间复杂度O(n^2)
+     *
+     * @param triangle
+     * @return
+     */
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int n = triangle.size();
+        int[][] dp = new int[n][n];
+        int r = 0;
+        int c = 0;
+        for (List<Integer> list : triangle) {
+            c = 0;
+            for (int num : list) {
+                dp[r][c++] = num;
+            }
+            r++;
+        }
+        int res = Integer.MAX_VALUE;
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                if (j == 0) {
+                    dp[i][j] += dp[i - 1][j];
+                } else if (j == i) {
+                    dp[i][j] += dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] += Math.min(dp[i - 1][j - 1], dp[i - 1][j]);
+                }
+                if (i == n - 1) {
+                    res = Math.min(res, dp[i][j]);
+                }
+            }
+        }
+        return res;
+    }
+
+    /**
+     * 空间复杂度O(n)
+     *
+     * @param triangle
+     * @return
+     */
+    public int minimumTotal2(List<List<Integer>> triangle) {
+        if (triangle == null || triangle.size() == 0) {
+            return 0;
+        }
+        int n = triangle.size();
+        if (n == 1) {
+            return triangle.get(0).get(0);
+        }
+        int[] dp = new int[n];
+        dp[0] = triangle.get(0).get(0);
+        for (int i = 1; i < n; i++) {
+            for (int j = i; j >= 0; j--) {
+                if (j == i) {
+                    dp[j] = dp[j - 1] + triangle.get(i).get(j);
+                } else if (j == 0) {
+                    dp[j] += triangle.get(i).get(j);
+                } else {
+                    dp[j] = Math.min(dp[j - 1] + triangle.get(i).get(j),
+                            dp[j] + triangle.get(i).get(j));
+                }
+            }
+        }
+        int res = dp[0];
+        for (int i = 1; i < n; i++) {
+            res = Math.min(res, dp[i]);
+        }
+        return res;
+    }
+}
---
+++ src/main/java/com/zjj/Leetcode/Leetcode127/Solution.java
@@ -0,0 +1,73 @@
+package com.zjj.Leetcode.Leetcode127;
+
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.ladderLength("hit", "cog",
+                new ArrayList<>(Arrays.asList("hot", "dot", "dog", "lot", "log", "cog"))));
+    }
+
+    class Pair {
+        String key;
+        int value;
+
+        public Pair(String key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        boolean flag = false;
+        for (String word : wordList) {
+            if (endWord.equals(word)) {
+                flag = true;
+            }
+        }
+        if (!flag) {
+            return 0;
+        }
+        int len = wordList.size();
+        boolean[] visit = new boolean[len];
+        Deque<Pair> deque = new ArrayDeque<>();
+        deque.addLast(new Pair(beginWord, 1));
+        while (!deque.isEmpty()) {
+            Pair p = deque.pollFirst();
+            String lastWord = p.key;
+            int step = p.value;
+            for (int i = 0; i < len; i++) {
+                if (!visit[i]) {
+                    String curWord = wordList.get(i);
+                    if (oneCharDiff(lastWord, curWord)) {
+                        visit[i] = true;
+                        if (endWord.equals(curWord)) {
+                            return step + 1;
+                        }
+                        deque.addLast(new Pair(curWord, step + 1));
+                    }
+                }
+            }
+        }
+        return 0;
+    }
+
+    private boolean oneCharDiff(String s1, String s2) {
+        if (s1.equals(s2)) {
+            return false;
+        }
+        int count = 0;
+        int len = s1.length();
+        for (int i = 0; i < len; i++) {
+            if (s1.charAt(i) != s2.charAt(i)) {
+                if (count == 1) {
+                    return false;
+                }
+                count++;
+            }
+        }
+        return count == 1;
+    }
+}
