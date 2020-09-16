96e574a64904cdf7634943625eee06830c4c563f
1277874614@qq.com <Zjj19961109>
2020-09-16 14:37:50 +0800

    leetcode

---
+++ src/main/java/com/zjj/Leetcode/Leetcode131/Solution.java
@@ -0,0 +1,49 @@
+package com.zjj.Leetcode.Leetcode131;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.partition("abbab"));
+    }
+
+    private List<List<String>> res;
+    private boolean[][] dp;
+
+    public List<List<String>> partition(String s) {
+        res = new ArrayList<>();
+        int len = s.length();
+        if (len == 0) {
+            return res;
+        }
+        dp = new boolean[len][len];
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j <= i; j++) {
+                if (s.charAt(j) == s.charAt(i) && (i - j <= 2 || dp[j + 1][i - 1])) {
+                    dp[j][i] = true;
+                }
+            }
+        }
+        backtrack(s, 0, len, new ArrayDeque<>());
+        return res;
+    }
+
+    private void backtrack(String s, int start, int length, Deque<String> deque) {
+        if (start == length) {
+            res.add(new ArrayList<>(deque));
+            return;
+        }
+        for (int i = start; i < length; i++) {
+            if (dp[start][i]) {
+                deque.addLast(s.substring(start, i + 1));
+                backtrack(s, i + 1, length, deque);
+                deque.pollLast();
+            }
+        }
+    }
+}
---
+++ src/main/java/com/zjj/Leetcode/Leetcode133/Solution.java
@@ -0,0 +1,67 @@
+package com.zjj.Leetcode.Leetcode133;
+
+import java.util.*;
+
+class Node {
+    public int val;
+    public List<Node> neighbors;
+
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+    }
+
+    public Node cloneGraph(Node node) {
+        if (node == null) {
+            return null;
+        }
+        Node clone = new Node(node.val, new ArrayList<>());
+        Map<Node, Node> cloneMap = new HashMap<>();
+        cloneMap.put(node, clone);
+        Deque<Node> deque = new ArrayDeque<>();
+        deque.addLast(node);
+        while (!deque.isEmpty()) {
+            Node cur = deque.pollLast();
+            for (Node n : cur.neighbors) {
+                if (!cloneMap.containsKey(n)) {
+                    cloneMap.put(n, new Node(n.val, new ArrayList<>()));
+                    deque.addLast(n);
+                }
+                cloneMap.get(cur).neighbors.add(cloneMap.get(n));
+            }
+        }
+        return clone;
+    }
+
+    Map<Node, Node> map = new HashMap<>();
+    public Node cloneGraph1(Node node) {
+        if(node == null) {
+            return null;
+        }
+        if(map.containsKey(node)) {
+            return map.get(node);
+        }
+        map.put(node, new Node(node.val, new ArrayList<>()));
+        for(Node p : node.neighbors) {
+            map.get(node).neighbors.add(cloneGraph(p));
+        }
+        return map.get(node);
+    }
+}
