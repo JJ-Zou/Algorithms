5c81902c0fd2287894afad4d048c15e8485c8578
zz Jenkin <1277874614@qq.com>
2020-09-23 23:04:58 +0800

    clean up

---
+++ src/main/java/com/zjj/test/leetcode/LinekedHashMap/TestLinkedHashMap.java
@@ -0,0 +1,20 @@
+package com.zjj.test.leetcode.LinekedHashMap;
+
+import com.zjj.Leetcode.listNode.ListNode;
+
+public class TestLinkedHashMap {
+
+    public static void main(String[] args) {
+        ListNode t1 = new ListNode(0);
+        t1.next = new ListNode(1);
+        ListNode temp = t1.next;
+        t1.next.next = new ListNode(2);
+        ListNode t2 = new ListNode(3);
+        t2.next = t1.next.next;
+        t1.next = t2;
+        System.out.println(t1);
+        System.out.println(temp);
+    }
+
+
+}
