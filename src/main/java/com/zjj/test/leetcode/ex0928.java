f0be5c653cc9981d8feb11e2162911ffa00207a6
1277874614@qq.com <Zjj19961109>
2020-09-29 10:38:37 +0800

    leetcode

---
+++ src/main/java/com/zjj/test/leetcode/ex0928.java
@@ -0,0 +1,41 @@
+package com.zjj.test.leetcode;
+
+public class ex0928 {
+    public static void main(String[] args) {
+//        int i = 0;
+//        int sum = 0;
+//        for (i = 3; i < 7; sum += i++) ;
+//        System.out.println(sum);
+        System.out.println(new ex0928().getDigits(99123435078L));
+        System.out.println(new ex0928().getAmount(1));
+        System.out.println(new ex0928().getAmount(2));
+        System.out.println(new ex0928().getAmount(3));
+        System.out.println(new ex0928().getAmount(4));
+        System.out.println(new ex0928().getAmount(5));
+        System.out.println(new ex0928().getAmount(6));
+        System.out.println(new ex0928().getAmount(7));
+    }
+    long getAmount(int m) {
+        if(m < 3) {
+            return 1;
+        }
+        if(m == 3) {
+            return 2;
+        }
+        int[] dp = new int[m];
+        dp[0] = 1;
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i = 3; i < m; i++) {
+            dp[i] = dp[i - 1] + dp[i - 3];
+        }
+        return dp[m - 1];
+    }
+    long getDigits(long value) {
+        String str = String.valueOf(value);
+        int len = str.length();
+        String s = "" + str.charAt(len - 3) + str.charAt(len - 5) + str.charAt(len - 7);
+        long l = Long.parseLong(s);
+        return l > 99 ? l : -1;
+    }
+}
--- src/main/java/com/zjj/test/leetcode/list/TestIterator.java
+++ src/main/java/com/zjj/test/leetcode/list/TestIterator.java
@@ -1,6 +1,7 @@
 package com.zjj.test.leetcode.list;
 
 import java.util.*;
+import java.util.concurrent.CountDownLatch;
 
 public class TestIterator {
     public static void main(String[] args) {
@@ -10,5 +11,8 @@ public class TestIterator {
         String[] arr = {"99", "90", "9", "932939"};
         Arrays.sort(arr, (o1, o2) -> (o2 + o1).compareTo(o1 + o2));
         System.out.println(Arrays.toString(arr));
+        Object[][] b = {{new Object(),},{new Object(), new Object()}};
+        System.out.println(b[0].length);
+        System.out.println(b[1].length);
     }
 }
