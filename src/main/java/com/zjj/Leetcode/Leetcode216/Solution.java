1387c285aaf06d04023474a225728db389da9bd4
zz Jenkin <1277874614@qq.com>
2020-09-30 14:49:36 +0800

    leetcode

---
+++ src/main/java/com/zjj/Leetcode/Leetcode216/Solution.java
@@ -0,0 +1,39 @@
+package com.zjj.Leetcode.Leetcode216;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+public class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.combinationSum3(2, 43));
+    }
+
+    private List<List<Integer>> res;
+
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        res = new ArrayList<>();
+        dfs(k, n, 1, new ArrayDeque<>());
+        return res;
+    }
+
+    private void dfs(int k, int n, int cur, Deque<Integer> deque) {
+        if (n < 0) {
+            return;
+        }
+        if (k == 0) {
+            if (n == 0) {
+                res.add(new ArrayList<>(deque));
+                return;
+            }
+            return;
+        }
+        for (int i = cur; i <= 9; i++) {
+            deque.addLast(i);
+            dfs(k - 1, n - i, i + 1, deque);
+            deque.pollLast();
+        }
+    }
+}
